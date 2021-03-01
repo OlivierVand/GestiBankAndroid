6204245c7baeb93ef5020c0475595b1cade55d88
OlivierVand <olivier.vandamme@live.fr>
2021-03-01 14:38:48 +0100

    ajoutPage

---
+++ app/src/main/java/com/sip/gestibank/Comptes.java
@@ -0,0 +1,14 @@
+package com.sip.gestibank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Comptes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comptes);
+    }
+}
