d4c7e4c08defece93cdf96699b1d24acc5d854e9
OlivierVand <olivier.vandamme@live.fr>
2021-03-01 18:15:55 +0100

    ajoutLayout

---
+++ app/src/main/java/com/sip/gestibank/ConvertionDevise.java
@@ -0,0 +1,14 @@
+package com.sip.gestibank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConvertionDevise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_convertion_devise);
+    }
+}
