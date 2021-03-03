4ce1ce96beb3ea88adb878ccacef0b2d65187239
OlivierVand <olivier.vandamme@live.fr>
2021-03-03 18:02:20 +0100

    modification des layouts

---
+++ app/src/main/java/com/sip/gestibank/CustomListView.java
@@ -0,0 +1,14 @@
+package com.sip.gestibank;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_list_view);
+    }
+}
